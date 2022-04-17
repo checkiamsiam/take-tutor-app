5056396507049bfd8bd8b9b4d8d9da57328651c8
checkiamsiam <issiam02415@gmail.com>
2022-04-17 16:53:44 +0600

    component adde

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,21 +4,24 @@ import  profile from '../../images/my-bc2.png';
 const About = () => {
   return (
     <div>
-      <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
-          <div class="max-h-96 md:h-screen">
-            <img class="w-screen h-screen object-cover object-top" src={profile} alt=""/>
+    <div className="container">
+    <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+          <div className="max-h-96 md:h-screen">
+            <img className="w-screen h-screen object-cover object-top" src={profile} alt=""/>
           </div>
-          <div class="flex bg-gray-100 p-10">
-            <div class="mb-auto mt-auto max-w-lg">
-              <h1 class="text-3xl uppercase">Jun Santilla</h1>
-              <p class="font-semibold mb-5">Web Developer</p>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-              <button class="bg-black rounded-md py-3 px-7 mt-6 text-white">Email Me</button>
+          <div className="flex bg-gray-100 p-10">
+            <div className="mb-auto mt-auto max-w-lg">
+              <h1 className="text-3xl uppercase">Sheikh Shahariar Siam</h1>
+              <p className="font-semibold mb-5">Web Developer</p>
+              <p>From starting of my web development career i always want to be a good developer first then i want to get a job in us or france but the best wish is very complecated but its amazing , to build a programing community with my friend.</p>
+              <button  className="bg-black rounded-md py-3 px-7 m-6 text-white z-10">Email Me</button> <br />
+              <code>this website is my creation as web developer i am not a tutor</code>
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
